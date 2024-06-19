86a6ba9630462fd5eb1b686383f85562c73fb21c
JavaOPs <admin@javaops.ru>
2024-06-19 20:15:09 +0300

    3_0_fix_TimeUtil

--- src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
@@ -8,7 +8,7 @@ public class DateTimeUtil {
     private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
     public static boolean isBetweenHalfOpen(LocalTime lt, LocalTime startTime, LocalTime endTime) {
-        return lt.compareTo(startTime) >= 0 && lt.compareTo(endTime) < 0;
+        return !lt.isBefore(startTime) && lt.isBefore(endTime);
     }
 
     public static String toString(LocalDateTime ldt) {
