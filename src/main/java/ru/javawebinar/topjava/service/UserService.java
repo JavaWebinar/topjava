cf3644a6a94365ced74c67bd6c123bc8d83fdd93
JavaOPs <admin@javaops.ru>
2026-02-12 15:23:24 +0300

    2_3_app_layers

---
+++ src/main/java/ru/javawebinar/topjava/service/UserService.java
@@ -0,0 +1,37 @@
+package ru.javawebinar.topjava.service;
+
+import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.repository.UserRepository;
+
+import java.util.List;
+
+import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFound;
+
+public class UserService {
+
+    private UserRepository repository;
+
+    public User create(User user) {
+        return repository.save(user);
+    }
+
+    public void delete(int id) {
+        checkNotFound(repository.delete(id), id);
+    }
+
+    public User get(int id) {
+        return checkNotFound(repository.get(id), id);
+    }
+
+    public User getByEmail(String email) {
+        return checkNotFound(repository.getByEmail(email), "email=" + email);
+    }
+
+    public List<User> getAll() {
+        return repository.getAll();
+    }
+
+    public void update(User user) {
+        checkNotFound(repository.save(user), user.getId());
+    }
+}
