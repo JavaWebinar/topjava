d3974a338bad04461ccdc448f61936c7464ba797
JavaOPs <admin@javaops.ru>
2026-03-12 00:44:09 +0300

    6_09_hibernate_cache

--- src/test/java/ru/javawebinar/topjava/service/jdbc/JdbcUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/jdbc/JdbcUserServiceTest.java
@@ -1,17 +1,12 @@
 package ru.javawebinar.topjava.service.jdbc;
 
 import org.junit.Ignore;
-import org.junit.Test;
 import org.springframework.test.context.ActiveProfiles;
 import ru.javawebinar.topjava.service.AbstractUserServiceTest;
 
 import static ru.javawebinar.topjava.Profiles.JDBC;
 
 @ActiveProfiles(JDBC)
+@Ignore
 public class JdbcUserServiceTest extends AbstractUserServiceTest {
-    @Override
-    @Ignore
-    @Test
-    public void createWithException() throws Exception {
-    }
 }
