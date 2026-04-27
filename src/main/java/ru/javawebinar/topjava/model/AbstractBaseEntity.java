22aeef3b27bad5b24b5776fff786304d94fe859a
JavaOPs <admin@javaops.ru>
2026-04-27 15:34:09 +0300

    11_16_HW_fix_swagger

--- src/main/java/ru/javawebinar/topjava/model/AbstractBaseEntity.java
+++ src/main/java/ru/javawebinar/topjava/model/AbstractBaseEntity.java
@@ -1,5 +1,6 @@
 package ru.javawebinar.topjava.model;
 
+import io.swagger.annotations.ApiModelProperty;
 import ru.javawebinar.topjava.HasId;
 
 import javax.persistence.*;
@@ -19,6 +20,7 @@ public abstract class AbstractBaseEntity implements HasId {
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "global_seq")
 //  See https://hibernate.atlassian.net/browse/HHH-3718 and https://hibernate.atlassian.net/browse/HHH-12034
 //  Proxy initialization when accessing its identifier managed now by JPA_PROXY_COMPLIANCE setting
+    @ApiModelProperty(hidden = true)
     protected Integer id;
 
     protected AbstractBaseEntity() {
